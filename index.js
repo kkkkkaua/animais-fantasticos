5111d392c279bcb685a0339a9f6f13fc955e335e
kkkkkaua <kauacoelho25@gmail.com>
2022-12-15 19:45:20 -0300

    end

--- index.js
+++ index.js
@@ -41,14 +41,49 @@ function initAccordion() {
 }
 initAccordion()
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
+function initSmoothScroll() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
 
-function scrollToSection(event) {
-  event.preventDefault()
-  const href = event.currentTarget.getAttribute('href')
-  const section = document.querySelector(href)
+  function scrollToSection(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href')
+    const section = document.querySelector(href)
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+
+    // forma alternativa
+    // const topo = section.offsetTop
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth'
+    // })
+  }
+
+  linksInternos.forEach(link => {
+    link.addEventListener('click', scrollToSection)
+  })
 }
+initSmoothScroll()
 
-linksInternos.forEach(link => {
-  link.addEventListener('click', scrollToSection)
-})
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll')
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6
+
+    function animaScroll() {
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top
+        const isVisibleSection = sectionTop - windowMetade < 0
+
+        if (isVisibleSection) section.classList.add('ativo')
+        else section.classList.remove('ativo')
+      })
+    }
+    animaScroll()
+    window.addEventListener('scroll', animaScroll)
+  }
+}
+initAnimacaoScroll()
